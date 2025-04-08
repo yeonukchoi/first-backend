ea24f5167d12ff95d841f148353bd48e38d2bf2c
yeonukchoi <chicyw0821@gmail.com>
2025-04-08 12:16:29 +0900

    게시판 목록 조회, 글 작성 기능

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 
+const posts = [];
+let id = 1;
+
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('게시판 실행중');
+});
+//게시판 목록 조회
+app.get('/posts', (req, res) => {
+    res.json(posts);
+});
+//게시판 글 작성
+app.post('/posts', (req, res) => {
+    const {title, content} = req.body;
+    const newPost = {id: id++, title, content, createdAt: new Date()};
+    posts.push(newPost);
+    res.status(201).json(newPost);
+});
 
 
 
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('서버가 3000번 포트에서 실행중입니다.');
+});
